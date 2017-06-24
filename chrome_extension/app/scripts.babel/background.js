600d87daffae3b109b790b1b872a0e3c74efc53f
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2017-06-24 16:40:55 -0400

    add dashboard

--- chrome_extension/app/scripts.babel/background.js
+++ chrome_extension/app/scripts.babel/background.js
@@ -1,11 +1,23 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(details => {
+  console.log('extension ready');
   console.log('previousVersion', details.previousVersion);
 });
+
+chrome.runtime.connect();
 
 chrome.tabs.onUpdated.addListener(tabId => {
   chrome.pageAction.show(tabId);
 });
 
-console.log('\'Allo \'Allo! Event Page for Page Action');
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.type == 'jobchange'){
+    chrome.notifications.create({
+      'type': 'basic',
+      'iconUrl': '../images/icon-128.png',
+      'title': 'Hi There!',
+      'message': 'It seems you just updated your job profile. Are you working at Google now? We have other 12 Per Schola alumnis working there. Contact them here!'
+    });
+  }
+});
--- chrome_extension/app/scripts.babel/contentscript.js
+++ chrome_extension/app/scripts.babel/contentscript.js
@@ -4,62 +4,37 @@ console.log('content scripts!');
 
 
 
+document.addEventListener('DOMContentLoaded', function () {
+  var icon = document.querySelector('.add-position');
+  console.log('loaded', icon);
 
-var classId = 'HTB-week-5';
-var classClickRefName = 'class/' + classId + '/clickCount';
+  if(icon == null){
+    setTimeout(function(){
+      icon = document.querySelector('.add-position');
 
-// Creates a firebase value watcher.
-// The callback gets called every time the click count changes
-function watchClickCount(callback) {
-  var clickCountRef = firebase.database().ref(classClickRefName);
-  clickCountRef.on('value', function(snapshot) {
-    var value = snapshot.val();
-    if (value === null) {
-      value = { count: 0 };
-    }
-    console.log('watched value:',value);
-    callback(value);
-  });
-}
+      icon.addEventListener('click', function(){
+        console.log('clicked');
 
-// Set up the watcher. Give it a callback that updates the browser
-// badge text whenever the count changes
-watchClickCount(function(value) {
-  var countString = '' + value.count;
-  chrome.browserAction.setBadgeText({text: countString});
-  if (value.count > 9999) {
-    resetCount();
+        setTimeout(function(){
+          var btnSubmit = document.querySelectorAll('.form-submit-action')[1];
+          console.log(btnSubmit)
+
+          btnSubmit.addEventListener('click', function(){
+            console.log('saved new job position');
+            setTimeout(function(){
+
+              chrome.runtime.sendMessage({type: 'jobchange'});
+              console.log('Hey you just changed job!');
+
+            }, 2000)
+          });
+
+        },2000);
+
+      });
+    }, 3000)
   }
-});
 
-function resetCount() {
-  writeClickCount(0);
-}
 
-// Reads the click count one time.
-// This is used to figure out the current count before incrementing it.
-function readClickCountOnce(callback) {
-  var clickCountRef = firebase.database().ref(classClickRefName);
-  clickCountRef.once('value').then(function(snapshot) {
-    var value = snapshot.val();
-    if (value === null) {
-      value = { count: 0 };
-    }
-    console.log('read value:',value);
-    callback(value);
-  });
-}
 
-function writeClickCount(int) {
-  console.log('setting click count to:',int);
-  var clickCountRef = firebase.database().ref(classClickRefName);
-  clickCountRef.set({count: int});
-}
-
-chrome.browserAction.onClicked.addListener(function() {
-  console.log('clicked');
-  readClickCountOnce(function(value) {
-    console.log('clicked ->',value.count);
-    writeClickCount(value.count + 1);
-  });
-});
+})
