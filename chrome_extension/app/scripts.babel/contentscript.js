cda5a114dd744fa102a1866cd96fb8bff5d0f5cc
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2017-06-24 14:14:21 -0400

    add dashboard

--- chrome_extension/app/scripts.babel/contentscript.js
+++ chrome_extension/app/scripts.babel/contentscript.js
@@ -1,3 +1,77 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+console.log('content scripts!');
+
+console.log('background for firebase clicks example');
+
+// Initialize Firebase
+// This uses my personal api key for firebase.
+// If you copy and paste this code, remember to create your OWN
+// free firebase app and replace this config with yours.
+var config = {
+	apiKey: "AIzaSyB4cHhG0lhQ_0QBhQX1M9pfHbfN-rx5k7s",
+	authDomain: "extension-backend.firebaseapp.com",
+	databaseURL: "https://extension-backend.firebaseio.com",
+	storageBucket: "extension-backend.appspot.com",
+	messagingSenderId: "778159737395"
+};
+firebase.initializeApp(config);
+
+var classId = 'HTB-week-5';
+var classClickRefName = 'class/' + classId + '/clickCount';
+
+// Creates a firebase value watcher.
+// The callback gets called every time the click count changes
+function watchClickCount(callback) {
+  var clickCountRef = firebase.database().ref(classClickRefName);
+  clickCountRef.on('value', function(snapshot) {
+    var value = snapshot.val();
+    if (value === null) {
+      value = { count: 0 };
+    }
+    console.log('watched value:',value);
+    callback(value);
+  });
+}
+
+// Set up the watcher. Give it a callback that updates the browser
+// badge text whenever the count changes
+watchClickCount(function(value) {
+  var countString = '' + value.count;
+  chrome.browserAction.setBadgeText({text: countString});
+  if (value.count > 9999) {
+    resetCount();
+  }
+});
+
+function resetCount() {
+  writeClickCount(0);
+}
+
+// Reads the click count one time.
+// This is used to figure out the current count before incrementing it.
+function readClickCountOnce(callback) {
+  var clickCountRef = firebase.database().ref(classClickRefName);
+  clickCountRef.once('value').then(function(snapshot) {
+    var value = snapshot.val();
+    if (value === null) {
+      value = { count: 0 };
+    }
+    console.log('read value:',value);
+    callback(value);
+  });
+}
+
+function writeClickCount(int) {
+  console.log('setting click count to:',int);
+  var clickCountRef = firebase.database().ref(classClickRefName);
+  clickCountRef.set({count: int});
+}
+
+chrome.browserAction.onClicked.addListener(function() {
+  console.log('clicked');
+  readClickCountOnce(function(value) {
+    console.log('clicked ->',value.count);
+    writeClickCount(value.count + 1);
+  });
+});
